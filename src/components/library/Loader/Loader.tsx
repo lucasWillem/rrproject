23d68f4dee472c8f190ad903ffa11581b84ecfef
Lucas van der Merwe <lucas.vandermerwe@blackswan.com>
2023-06-05 18:26:17 +0200

    add library - including Loader component

---
+++ src/components/library/Loader/Loader.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from "react";
+import { Variant } from "../../../redux/loaderSlice";
+import { StyledLoader } from "./styles";
+
+interface LoaderProps {
+  isVisible: boolean;
+  variant?: Variant;
+}
+
+const Loader: FC<LoaderProps> = ({ isVisible, variant='inherit' }) => {
+  return isVisible ? <StyledLoader color={variant} /> : null;
+};
+
+export { Loader };
---
+++ src/components/library/Loader/styles.ts
@@ -0,0 +1,9 @@
+import { CircularProgress, styled } from "@mui/material";
+
+const StyledLoader = styled(CircularProgress)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+}));
+
+export { StyledLoader };

fdeb28a90a3800c03d00dd499aa86004acaf9984
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-04-08 17:40:08 +0200

    feature: add Country and ListOfCountries pages

---
+++ src/pages/CountryPage.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+import DetailedCountry from "../components/containers/DetailedCountry";
+
+export const CountryPage: FC = () => {
+  return (
+    <DetailedCountry />
+  );
+};

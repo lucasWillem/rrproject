3a34d7c1e3fd1abbdc33d1c97e889dd302ff1554
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-04-08 16:43:57 +0200

    feature: add CountryDatumItem component

---
+++ src/components/library/CountryDatumItem/CountryDatumItem.tsx
@@ -0,0 +1,21 @@
+import React, { FC, ReactNode } from "react";
+import { StyledCountryDatumItem } from "./styles";
+import { TypographyVariant } from "@mui/material";
+
+interface CountryDatumItemProps {
+  icon: ReactNode;
+  items: any[];
+  renderItem: (item: any, i: number, arr: any[], typographyVariant: TypographyVariant) => JSX.Element;
+  typographyVariant: TypographyVariant
+}
+
+const CountryDatumItem: FC<CountryDatumItemProps> = ({ icon, items, renderItem, typographyVariant }) => {
+  return <StyledCountryDatumItem>
+    {icon}
+    {items.map((item, i, arr) => renderItem(item, i, arr, typographyVariant))}
+  </StyledCountryDatumItem>
+};
+
+export { CountryDatumItem };
+
+
---
+++ src/components/library/CountryDatumItem/styles.ts
@@ -0,0 +1,5 @@
+import { Box, styled } from "@mui/material";
+
+const StyledCountryDatumItem = styled(Box)(({ theme }) => ({ display: 'flex', alignItems: 'center', gap: theme.spacing(1), margin: theme.spacing(1) }));
+
+export { StyledCountryDatumItem };

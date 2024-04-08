8f65ddf79ca365c28a4155ee4552fdaaa2e16234
Lucas van der Merwe <lucas.willem.vdm@gmail.com>
2024-04-08 17:10:46 +0200

    feature: add Loader and Loader with overlay library components

--- src/components/library/Loader/styles.ts
+++ src/components/library/Loader/styles.ts
@@ -1,9 +1,9 @@
 import { CircularProgress, styled } from "@mui/material";
 
 const StyledLoader = styled(CircularProgress)(({ theme }) => ({
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
 }));
 
 export { StyledLoader };
